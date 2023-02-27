0a80cb5b65768fcca02f84c44d55eff202ef72c0
Juanjo <gonzalezsevilla@gmail.com>
2023-02-27 11:15:28 +0100

    fin Drag and Drop To Do List

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { AppComponent } from './app.component';
+import { TareaComponent } from './components/tarea/tarea.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TareaComponent
+  ],
+  imports: [
+    BrowserModule,
+    DragDropModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
